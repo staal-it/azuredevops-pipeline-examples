d985ce2f4c0f2e99c9fca2109d6522cbbba9da0d
Erwin Staal <info@staal-it.nl>
2022-06-15 14:47:38 +0200

    Feature/upgrade pipeline scripts AB#422 (#3)
    * Upgrade pipeline scripts
    
    Co-authored-by: Erwin Staal <estaal@xpirit.com>

--- azure-terraform/init-tf.sh
+++ azure-terraform/init-tf.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -xe
 
-. ./common-tf.sh 1
+TENANT_ID=$(az account show --query tenantId -o tsv)
+SUBSCRIPTION_ID=$(az account show --query id -o tsv)
+storage_account_name=stterraformdemoterradev
+STATE_RESOURCEGROUP_NAME=rg-terraformdemo-terra-dev
 
-export storage_account_name=estfdemostatedev
-
-terraform init \
+terraform init -upgrade \
   -backend-config=storage_account_name=$storage_account_name \
-  -backend-config=container_name=terraform-state \
+  -backend-config=container_name=terraformstate \
   -backend-config=key=terraform.tfstate \
   -backend-config=resource_group_name=$STATE_RESOURCEGROUP_NAME \
   -backend-config=tenant_id=$TENANT_ID \
--- azure-terraform/locals.tf
+++ azure-terraform/locals.tf
@@ -1,6 +1,4 @@
 locals {
-  environment_file       = yamldecode(file(var.ENVIRONMENT_YML))
-
-  storage_account_name = "terraform-example-${local.environment_file.name}"
+  storage_account_name = "terraform-example-${var.name}"
   vnet_address_space = "192.168.1.0/24"
 }
